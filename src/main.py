5b64e55602f24389d93e555d299448d0390a243f
JZims <zimmerman.j@hotmail.com>
2025-02-27 17:20:36 -0500

    scaffolding done

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode, TextType
+
+def main():
+    text_node = TextNode("Hello World!", TextType.TEXT, "lul.com")
+    print(text_node)
+
+if __name__ =="__main__":
+    main()
