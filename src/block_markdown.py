cb91ac505c8d0c806dcb492366d206f503ce45d9
JZims <zimmerman.j@hotmail.com>
2025-03-31 01:29:20 -0400

    very base case md to html working

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,5 +1,7 @@
 from enum import Enum
 from htmlnode import HTMLNode, ParentNode, LeafNode
+from inline_markdown import text_to_textnodes
+from textnode import TextType, text_node_to_html_node
 import re
 
 class BlockType(Enum):
@@ -21,7 +23,7 @@ def markdown_to_blocks(markdown):
     return filtered_blocks
 
 def block_to_block_type(block):
-    heading=re.match(r'^#{3,6}\s', block)
+    heading=re.match(r'^#{1,6}\s', block)
     if heading:
         return BlockType("h1")
     code=re.match(r'^```[\s\S]*```$', block)
@@ -40,17 +42,26 @@ def block_to_block_type(block):
         return BlockType("p")
     
 
+def text_to_children(markdown):
+    all_text_nodes=text_to_textnodes(markdown)
+    child_nodes=[]
+    for node in all_text_nodes:
+        if node.text_type != TextType.TEXT:
+            child_nodes.append(text_node_to_html_node(node).to_html())
+    return child_nodes
+
 def markdown_to_html_node(markdown):
     blocks = markdown_to_blocks(markdown)
     block_with_types = list(zip(blocks,map(lambda x: block_to_block_type(x).value, blocks)))
-    html_nodes=[]
-    for block in block_with_types:
+    child_nodes=[]
+    wrapper = ParentNode('div', child_nodes)
 
+    for block in block_with_types:
+        stylized_text = "".join(list(map(lambda x: x.to_html(), map(lambda x: text_node_to_html_node(x), text_to_textnodes(block[0])))))
+        child_nodes.append(LeafNode(block[1], stylized_text))
         # Determine which are parent nodes and which are leaf nodes
-        # html_node = LeafNode(block[1], block[0]).to_html()
-        # html_nodes.append(html_node)
-   
-    return html_nodes
+
+    return wrapper.to_html()
 
 md = """
 This is **bolded** paragraph
