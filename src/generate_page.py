3dc10dcfc303236f385f2d824e8037cc21ceae1e
JZims <zimmerman.j@hotmail.com>
2025-04-09 01:06:18 -0400

    updated for ghub deploy

--- src/generate_page.py
+++ src/generate_page.py
@@ -3,17 +3,17 @@ import os
 from pathlib import Path
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath="/"):
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
         if os.path.isfile(from_path):
             dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_path)
+            generate_page(from_path, template_path, dest_path, basepath)
         else:
             generate_pages_recursive(from_path, template_path, dest_path)
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(F"Generating a page from {from_path} to {dest_path}.")
     with open(from_path, "r") as source_md:
         read_data = source_md.read()
@@ -25,6 +25,8 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(from_path)
     temp_data = temp_data.replace("{{ Title }}", title)
     temp_data = temp_data.replace("{{ Content }}", html)
+    temp_data = temp_data.replace("href=\"/", f"href=\"{basepath}")
+    temp_data = temp_data.replace("src=\"/", f"src=\"{basepath}")
 
     # Ensure directory exists
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
--- src/main.py
+++ src/main.py
@@ -2,11 +2,13 @@
 from copy_static import copy_static_files
 import shutil, os
 from generate_page import generate_page, generate_pages_recursive
+import sys
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
+basepath=sys.argv[0]
 
 def main():
 
@@ -18,7 +20,9 @@ def main():
     copy_static_files(dir_path_static, dir_path_public)
 
     print("Generating content")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
+
+
     
 
 if __name__ == "__main__":
